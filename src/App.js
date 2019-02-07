2b3e6eb901b23bd8c64a12ae5de4b2daa94635ef
Elle Hallal <daniellehallal@gmail.com>
2019-02-07 14:13:51 +0000

    Test componentDidMount()

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import SeasonDisplay from "./components/SeasonDisplay";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      lat: null,
+      long: null,
+      errorMessage: ""
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      function(location) {
+        this.setState({
+          lat: location.coords.latitude,
+          lon: location.coords.longitude
+        });
+      }.bind(this),
+      function(err) {
+        console.log(err);
+        this.setState({
+          errorMessage: "Unable to get location"
+        });
+      }.bind(this)
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
-
     return (
       <div className="App">
-        Latitude
+        <p data-test="latitude">Latitude: {this.state.lat}</p>
+        <p data-test="longitude">Longitude: {this.state.lon}</p>
+        <p data-test="error-message">{this.state.errorMessage}</p>
         <SeasonDisplay />
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
@@ -12,6 +12,36 @@ const mockGeolocation = {
 
 global.navigator.geolocation = mockGeolocation;
 
+let wrapper;
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
 test("renders without crashing", () => {
   shallow(<App />);
 });
+
+test("componentDidMount", () => {
+  const spy = jest.spyOn(App.prototype, "componentDidMount");
+  const wrapper = mount(<App />);
+  wrapper.instance().componentDidMount();
+  expect(spy).toHaveBeenCalled();
+
+  // const instance = wrapper.instance();
+  // instance.componentDidMount();
+  // expect(instance.componentDidMount().ca).toBe(true);
+});
+
+// test("displays the longitude", () => {
+//   const longitude = findByTestAttr(wrapper, "longitude");
+//   expect(longitude.text()).toContain(".");
+// });
+
+// test("displays the latitude", () => {
+//   const latitude = findByTestAttr(wrapper, "latitude");
+//   expect(latitude.text()).toContain(".");
+// });
